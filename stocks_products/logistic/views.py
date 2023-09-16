37924cc2e01e4ee74d47491201cac8a8d0f34ff6
Alex Or <orlov.alexx88@gmail.com>
2023-09-16 15:57:52 +0300

    add

---
+++ stocks_products/logistic/views.py
@@ -0,0 +1,22 @@
+from rest_framework import filters
+from rest_framework.viewsets import ModelViewSet
+
+from .models import Product, Stock
+from .serializers import ProductSerializer, StockSerializer
+
+
+class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    # при необходимости добавьте параметры фильтрации
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['title', 'description']
+
+
+class StockViewSet(ModelViewSet):
+    queryset = Stock.objects.all()
+    serializer_class = StockSerializer
+    # при необходимости добавьте параметры фильтрации
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['products']
+    filterset_fields = ['products']
